bffe38e093838da6708661139627f6ef6a90646e
Yosua Tresno Surojo <yosua1011@gmail.com>
2017-02-20 09:31:27 +0700

    Ignoring Failing Tests

---
+++ unittest_expectedfailure.py
@@ -0,0 +1,15 @@
+# unittest_expectedfailure.py
+
+import unittest
+
+
+class Test(unittest.TestCase):
+
+	@unittest.expectedFailure
+	def test_never_passes(self):
+		self.assertTrue(False)
+
+	@unittest.expectedFailure
+	def test_always_passes(self):
+		self.assertTrue(True)
+
